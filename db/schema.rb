91e591e0d5ab26a5471ceef6938d55973bce0512
Thiago Araújo <thiagosilva.java@email.com>
2020-02-21 07:07:11 -0300

    :rocket: configura o banco de dados postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_20_132757) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jobs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
